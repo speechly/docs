88c71b880cf7a822f602a5f6adfb9669cb29a7fc
Ari Nykänen <ari.o.nykanen@gmail.com>
2021-02-17 15:33:29 +0200

    Better cURL error handling in script

--- build-docs.sh
+++ build-docs.sh
@@ -1,9 +1,15 @@
+CURL_OPTS="--fail --silent --show-error"
+
 # ----------- Build -----------------
 TARGET=_index.md
 TARGET_NAME=_index.md
 TARGET_PATH=content/client-libraries/react/ui-components
+SOURCE=https://raw.githubusercontent.com/speechly/react-ui/main/index.md
 
-echo Building $TARGET_PATH/$TARGET_NAME...
+echo Building \"$TARGET_PATH/$TARGET_NAME\" from \"$SOURCE\"...
 
 cat $TARGET_PATH/$TARGET_NAME.header > $TARGET_PATH/$TARGET_NAME
-curl https://raw.githubusercontent.com/speechly/react-ui/main/index.md >> $TARGET_PATH/$TARGET_NAME
+curl $CURL_OPTS $SOURCE >> $TARGET_PATH/$TARGET_NAME
+
+# ----------- Build -----------------
+echo OK

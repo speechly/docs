ea6b1d9fc5ae905ed08ad4d539b0aa500ae24151
Markus Lång <markus.lang@iki.fi>
2021-03-12 13:51:19 +0200

    Update API reference from github resources

--- build-docs.sh
+++ build-docs.sh
@@ -1,4 +1,4 @@
-CURL_OPTS="--fail --silent --show-error"
+CURL="curl --fail --silent --show-error"
 
 # ----------- Build -----------------
 TARGET=_index.md
@@ -9,7 +9,14 @@ SOURCE=https://raw.githubusercontent.com/speechly/react-ui/main/index.md
 echo Building \"$TARGET_PATH/$TARGET_NAME\" from \"$SOURCE\"...
 
 cat $TARGET_PATH/$TARGET_NAME.header > $TARGET_PATH/$TARGET_NAME
-curl $CURL_OPTS $SOURCE >> $TARGET_PATH/$TARGET_NAME
+$CURL $SOURCE >> $TARGET_PATH/$TARGET_NAME
 
 # ----------- Build -----------------
 echo OK
+
+APIREF=content/speechly-api/api-reference.md
+API_HEADER=content/speechly-api/api-reference.header
+echo Building API reference to $APIREF
+cp $API_HEADER $APIREF
+$CURL https://raw.githubusercontent.com/speechly/api/master/docs/slu.md >> $APIREF
+$CURL https://raw.githubusercontent.com/speechly/api/master/docs/identity.md >> $APIREF
